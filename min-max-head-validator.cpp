a2dedc88baf1e322140e6fe9bae36b8e1d74abf0
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-07-18 16:38:50 -0400

    Added max heap function

--- min-max-head-validator.cpp
+++ min-max-head-validator.cpp
@@ -14,3 +14,11 @@ bool isMinHeap(const vector<int>& arr) {
     }
     return true; 
 }
+
+
+bool isMaxHeap(const vector<int>& arr) {
+    int n = arr.size(); // Getting the size of the array
+    for (int i = 0; i <= (n - 2) / 2; i++) { // Loop through each element up to the parent of the last element
+        // If left child is greater, return false
+        if (2 * i + 1 < n && arr[i] < arr[2 * i + 1]) // Checking if left child exists and greater than the parent
+            return false;
