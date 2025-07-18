fad92472d1ddb4e8637d4ebabfde0c06e0ebcab5
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-07-18 16:38:27 -0400

    Added last bit of min heap function

--- min-max-head-validator.cpp
+++ min-max-head-validator.cpp
@@ -9,3 +9,8 @@ bool isMinHeap(const vector<int>& arr) {
     for (int i = 0; i <= (n - 2) / 2; i++) { 
         if (2 * i + 1 < n && arr[i] > arr[2 * i + 1]) 
             return false;
+        if (2 * i + 2 < n && arr[i] > arr[2 * i + 2]) 
+            return false;
+    }
+    return true; 
+}
