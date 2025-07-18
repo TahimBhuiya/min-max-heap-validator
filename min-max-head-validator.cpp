e10f20d5871c4c522bb1102cabd36a672eaa2c1a
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-07-18 16:37:45 -0400

    Added min heap function

--- min-max-head-validator.cpp
+++ min-max-head-validator.cpp
@@ -3,3 +3,9 @@
 #include <vector>  
 
 using namespace std;
+
+bool isMinHeap(const vector<int>& arr) {
+    int n = arr.size(); 
+    for (int i = 0; i <= (n - 2) / 2; i++) { 
+        if (2 * i + 1 < n && arr[i] > arr[2 * i + 1]) 
+            return false;
